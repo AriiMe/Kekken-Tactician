1b935c2b1e6a6b7769da9a8157ffee9c20a24298
Ari Razab <arianrazab@yahoo.com>
2024-05-01 16:20:57 +0200

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react";
+import { CombinedProvider } from "./context/CombinedProvider";
 import "./index.css";
-import { CombinedProvider } from "./context/CombinedProvider"; // Updated import
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <CombinedProvider>
     <App />
+    <Analytics />
   </CombinedProvider>
 );
