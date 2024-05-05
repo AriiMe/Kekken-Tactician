904ecc0e113955cea1c51988f18294b6a55e156d
mhdalihoria <alihoria123456@gmail.com>
2024-05-05 14:36:29 +0300

    Update Request Page Updated

--- src/pages/UpdateRequest.jsx
+++ src/pages/UpdateRequest.jsx
@@ -4,6 +4,20 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
 
+const paragraphStyle = {
+  fontSize: "1rem",
+  textAlign: "left",
+  width: "71%",
+  margin: "1rem auto",
+  fontFamily: "Michroma",
+  letterSpacing: "1px",
+  lineHeight: "2",
+  "@media (max-width: 1100px)": {
+    fontSize: ".8rem",
+    width: "90%",
+  },
+};
+
 const UpdateRequest = () => {
   return (
     <Container maxWidth={false}>
@@ -19,28 +33,39 @@ const UpdateRequest = () => {
         }}
       >
         <Typography
-          variant="h2"
+          variant="h3"
           component="h1"
           gutterBottom
           align="center"
-          sx={{ color: "rgba(212, 47, 47, 1)", marginBottom: "50px" }}
+          sx={{
+            color: "rgba(212, 47, 47, 1)",
+            fontFamily: "Michroma",
+            width: "70%",
+            margin: "0 auto 4rem",
+            "@media (max-width: 1100px)": {
+              fontSize: "2rem",
+              width: "90%",
+            },
+          }}
         >
           Request Character Updates
         </Typography>
-        <Typography variant="body1" paragraph sx={{ fontSize: "20px" }}>
+        <Typography variant="body1" paragraph sx={paragraphStyle}>
           If you see a scrub wrote combos for your main, let me know! I'll
-          gladly update the combos. Reach out to @ariime on Discord and tell me
-          what you need. Your feedback and contributions help Tekken Tactician
-          stay fresh and valuable for the entire community.
+          gladly update the combos. Reach out to{" "}
+          <span style={{ color: "#d42f2f" }}>@ariime</span> on Discord and tell
+          me what you need. Your feedback and contributions help Tekken
+          Tactician stay fresh and valuable for the entire community.
         </Typography>
-        <Typography variant="body1" paragraph sx={{ fontSize: "20px" }}>
+        <Typography variant="body1" paragraph sx={paragraphStyle}>
           Alternatively, if you want to show some love and help me work faster,
-          consider donating to my Ko-fi page. Your support is greatly
-          appreciated and helps me dedicate more resources to improve and expand
-          the content. As a bonus the performance of the site will be
+          consider donating to my
+          <span style={{ color: "#d42f2f" }}> Ko-fi </span>page. Your support is
+          greatly appreciated and helps me dedicate more resources to improve
+          and expand the content. As a bonus the performance of the site will be
           drastically improved.
         </Typography>
-        <Typography variant="body1" paragraph sx={{ fontSize: "20px" }}>
+        <Typography variant="body1" paragraph sx={paragraphStyle}>
           Only people with massive PP donate on
           <Link
             href="https://ko-fi.com/ariime"
